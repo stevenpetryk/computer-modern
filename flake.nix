771afcdeba81cc9392d0cfd433025fc7fe459878
Steven Petryk <petryk.steven@gmail.com>
2024-10-14 13:45:50 -0700

    Add flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Mafs' developer environment";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.nodejs_22
+            pkgs.woff2
+
+            (pkgs.writeScriptBin "pnpm" ''
+              corepack pnpm "$@"
+            '')
+          ];
+        };
+      });
+}
